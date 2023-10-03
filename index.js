998ebdd8957e66134ce2326f31f534fbfb6eee8f
Theresa Tran <theresa1194@gmail.com>
2023-10-03 22:24:34 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Theresa is the champion';
